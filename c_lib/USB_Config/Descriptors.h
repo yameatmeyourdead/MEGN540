6d5776565ee3baff735292217ddb1e0a390081c5
yameatmeyourdead <zac71113@gmail.com>
2024-03-17 10:20:43 -0600

    increase ring buffer and end point size

--- c_lib/USB_Config/Descriptors.h
+++ c_lib/USB_Config/Descriptors.h
@@ -54,7 +54,7 @@
  *  MEGN540
  * Copyright (c) Andrew Petruska 2020
  * modifications for mechctronics class
- * 
+ *
  */
 
 /** \file
@@ -65,78 +65,70 @@
 #ifndef _DESCRIPTORS_H_
 #define _DESCRIPTORS_H_
 
-	/* Includes: */
-        //#include "LUFAConfig.h"
-		#include <LUFA/Drivers/USB/USB.h>
-		#include <avr/pgmspace.h>
+/* Includes: */
+//#include "LUFAConfig.h"
+#include <LUFA/Drivers/USB/USB.h>
+#include <avr/pgmspace.h>
 
-	/* Macros: */
-		/** Endpoint address of the first CDC interface's device-to-host data IN endpoint. */
-		#define CDC_TX_EPADDR                 (ENDPOINT_DIR_IN  | 1) 
+/* Macros: */
+/** Endpoint address of the first CDC interface's device-to-host data IN endpoint. */
+#define CDC_TX_EPADDR ( ENDPOINT_DIR_IN | 1 )
 
-		/** Endpoint address of the first CDC interface's host-to-device data OUT endpoint. */
-		#define CDC_RX_EPADDR                 (ENDPOINT_DIR_OUT | 2) 
+/** Endpoint address of the first CDC interface's host-to-device data OUT endpoint. */
+#define CDC_RX_EPADDR ( ENDPOINT_DIR_OUT | 2 )
 
-		/** Endpoint address of the first CDC interface's device-to-host notification IN endpoint. */
-		#define CDC_NOTIFICATION_EPADDR       (ENDPOINT_DIR_IN  | 3) 
+/** Endpoint address of the first CDC interface's device-to-host notification IN endpoint. */
+#define CDC_NOTIFICATION_EPADDR ( ENDPOINT_DIR_IN | 3 )
 
+/** Size in bytes of the CDC device-to-host notification IN endpoints. */
+#define CDC_NOTIFICATION_EPSIZE 8
 
-		/** Size in bytes of the CDC device-to-host notification IN endpoints. */
-		#define CDC_NOTIFICATION_EPSIZE        8
+/** Size in bytes of the CDC data IN and OUT endpoints. */
+#define CDC_TXRX_EPSIZE 32
 
-		/** Size in bytes of the CDC data IN and OUT endpoints. */
-		#define CDC_TXRX_EPSIZE                16
+/* Type Defines: */
+/** Type define for the device configuration descriptor structure. This must be defined in the
+ *  application code, as the configuration descriptor contains several sub-descriptors which
+ *  vary between devices, and which describe the device's usage to the host.
+ */
+typedef struct {
+    USB_Descriptor_Configuration_Header_t Config;
 
-	/* Type Defines: */
-		/** Type define for the device configuration descriptor structure. This must be defined in the
-		 *  application code, as the configuration descriptor contains several sub-descriptors which
-		 *  vary between devices, and which describe the device's usage to the host.
-		 */
-		typedef struct
-		{
-			USB_Descriptor_Configuration_Header_t    Config;
+    // First CDC Control Interface
+    USB_Descriptor_Interface_t CDC_CCI_Interface;
+    USB_CDC_Descriptor_FunctionalHeader_t CDC_Functional_Header;
+    USB_CDC_Descriptor_FunctionalACM_t CDC_Functional_ACM;
+    USB_CDC_Descriptor_FunctionalUnion_t CDC_Functional_Union;
+    USB_Descriptor_Endpoint_t CDC_ManagementEndpoint;
 
-			// First CDC Control Interface
-			USB_Descriptor_Interface_t               CDC_CCI_Interface;
-			USB_CDC_Descriptor_FunctionalHeader_t    CDC_Functional_Header;
-			USB_CDC_Descriptor_FunctionalACM_t       CDC_Functional_ACM;
-			USB_CDC_Descriptor_FunctionalUnion_t     CDC_Functional_Union;
-			USB_Descriptor_Endpoint_t                CDC_ManagementEndpoint;
+    // First CDC Data Interface
+    USB_Descriptor_Interface_t CDC_DCI_Interface;
+    USB_Descriptor_Endpoint_t CDC_DataOutEndpoint;
+    USB_Descriptor_Endpoint_t CDC_DataInEndpoint;
 
-			// First CDC Data Interface
-			USB_Descriptor_Interface_t               CDC_DCI_Interface;
-			USB_Descriptor_Endpoint_t                CDC_DataOutEndpoint;
-			USB_Descriptor_Endpoint_t                CDC_DataInEndpoint;
+} USB_Descriptor_Configuration_t;
 
-		} USB_Descriptor_Configuration_t;
+/** Enum for the device interface descriptor IDs within the device. Each interface descriptor
+ *  should have a unique ID index associated with it, which can be used to refer to the
+ *  interface from other descriptors.
+ */
+enum InterfaceDescriptors_t {
+    INTERFACE_ID_CDC_CCI = 0, /**< CDC1 CCI interface descriptor ID */
+    INTERFACE_ID_CDC_DCI = 1, /**< CDC1 DCI interface descriptor ID */
+};
 
-		/** Enum for the device interface descriptor IDs within the device. Each interface descriptor
-		 *  should have a unique ID index associated with it, which can be used to refer to the
-		 *  interface from other descriptors.
-		 */
-		enum InterfaceDescriptors_t
-		{
-			INTERFACE_ID_CDC_CCI = 0, /**< CDC1 CCI interface descriptor ID */
-			INTERFACE_ID_CDC_DCI = 1, /**< CDC1 DCI interface descriptor ID */
-		};
+/** Enum for the device string descriptor IDs within the device. Each string descriptor should
+ *  have a unique ID index associated with it, which can be used to refer to the string from
+ *  other descriptors.
+ */
+enum StringDescriptors_t {
+    STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
+    STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
+    STRING_ID_Product      = 2, /**< Product string ID */
+};
 
-		/** Enum for the device string descriptor IDs within the device. Each string descriptor should
-		 *  have a unique ID index associated with it, which can be used to refer to the string from
-		 *  other descriptors.
-		 */
-		enum StringDescriptors_t
-		{
-			STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
-			STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
-			STRING_ID_Product      = 2, /**< Product string ID */
-		};
-
-	/* Function Prototypes: already defined in Device.h included through USB.h */
-		uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
-		                                    const uint16_t wIndex,
-		                                    const void** const DescriptorAddress)
-		                                    ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
-
+/* Function Prototypes: already defined in Device.h included through USB.h */
+uint16_t CALLBACK_USB_GetDescriptor( const uint16_t wValue, const uint16_t wIndex, const void** const DescriptorAddress ) ATTR_WARN_UNUSED_RESULT
+    ATTR_NON_NULL_PTR_ARG( 3 );
 
 #endif
-
